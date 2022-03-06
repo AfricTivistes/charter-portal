f9052d284ebfd315e8114ef2951edbd598a2beb0
Mamadou Diagne <genova@dofbi.com>
2022-03-06 17:20:28 +0000

    ♻️ refactor(changeLocale): add useEffect

--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import GlobalStyles from '../styles/global';
 
 import Header from '../components/Header';
@@ -12,7 +12,12 @@ const BaseLayout = ({ children, pageContext: { locale } }) => {
   // Using the useLocale() hook to define the correct locale 
   // that will be available in all components of the tree thought its context
   const { changeLocale } = useLocale();
-  changeLocale(locale);
+  
+  useEffect(() => {
+    if (locale) {
+      changeLocale(locale);
+    }
+  }, [locale])
 
   return (
     <>
